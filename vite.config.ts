4ab94cb1e0421d45684c0297846a552a050c69b3
keshavgoel2023 <156510526+keshavgoel2023@users.noreply.github.com>
2025-08-06 18:41:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/", // 👈 THIS IS CORRECT for USERNAME.github.io (do NOT change it)
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
